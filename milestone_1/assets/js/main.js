b960f42d16c0329d8e20fb5b6b6729feb943590d
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-06-08 11:52:17 +0200

    add seraching function on-click

--- milestone_1/assets/js/main.js
+++ milestone_1/assets/js/main.js
@@ -1,21 +1,31 @@
 const app = new Vue({
     el: "#app",
     data: {
-        url: 'https://api.themoviedb.org/3/search/movie?api_key=0d8074b8bfe08af5bfdf6766d8596223&query='
+        url: 'https://api.themoviedb.org/3/search/movie?api_key=0d8074b8bfe08af5bfdf6766d8596223&query=',
+        searchString: "",
     },
 
     methods: {
+        searching(){
+            let fullUrl = this.url + this.searchString;
 
+            axios
+                .get(fullUrl)
+                .then(result => {
+                    console.log(result.data.results);
+                })
+                .catch(e => {
+                    console.log(e);
+                })
+
+        }
     },
 
     mounted() {
-        axios
-        .get(this.url)
-        .then(result =>{
-            console.log(result.data.results);
-        })
-        .catch( e =>{
-            console.log(e);
-        })
     }
 })
+
+/* titolo
+titolo originale
+lingua
+voto */
