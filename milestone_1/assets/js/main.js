f37ce7acc182a6ea8ef829d90a129c493a44413f
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-06-08 11:32:17 +0200

    inital axios call

--- milestone_1/assets/js/main.js
+++ milestone_1/assets/js/main.js
@@ -1,7 +1,7 @@
 const app = new Vue({
     el: "#app",
     data: {
-
+        url: 'https://api.themoviedb.org/3/search/movie?api_key=0d8074b8bfe08af5bfdf6766d8596223&query='
     },
 
     methods: {
@@ -9,6 +9,13 @@ const app = new Vue({
     },
 
     mounted() {
-        
+        axios
+        .get(this.url)
+        .then(result =>{
+            console.log(result.data.results);
+        })
+        .catch( e =>{
+            console.log(e);
+        })
     }
 })
