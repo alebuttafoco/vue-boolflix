602328a523fc5294191994bcd9d90cc6fb88b021
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-06-08 14:17:36 +0200

    add seraching tv series

--- milestone_2/assets/js/main.js
+++ milestone_2/assets/js/main.js
@@ -1,25 +1,35 @@
 const app = new Vue({
     el: "#app",
     data: {
-        url: 'https://api.themoviedb.org/3/search/movie?api_key=0d8074b8bfe08af5bfdf6766d8596223&query=',
+        urlFilms: 'https://api.themoviedb.org/3/search/movie?api_key=0d8074b8bfe08af5bfdf6766d8596223&query=',
+        urlSeries: 'https://api.themoviedb.org/3/search/tv?api_key=0d8074b8bfe08af5bfdf6766d8596223&query=',
+        filmsData: [],  
+        seriesData: [],
         searchString: "",
-        filmsData: [],        
     },
 
     methods: {
         searching(){
-            let fullUrl = this.url + this.searchString;
+            let fullUrlFilms = this.urlFilms + this.searchString;
+            let fullUrlSeries = this.urlSeries + this.searchString;
 
             axios
-                .get(fullUrl)
-                .then(result => {
-                    this.filmsData = result.data.results;
-                    console.log(this.filmsData);
-                })
-                .catch(e => {
-                    console.log(e);
-                })
+            .get(fullUrlFilms)
+            .then(result => {
+                this.filmsData = result.data.results;
+            })
+            .catch(e => {
+                console.log(e);
+            })
 
+            axios
+            .get(fullUrlSeries)
+            .then(result => {
+                this.seriesData = result.data.results;
+            })
+            .catch(e => {
+                console.log(e);
+            })
         }
     },
 
